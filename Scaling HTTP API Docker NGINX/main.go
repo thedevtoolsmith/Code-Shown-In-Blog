cf04f47b593646932980d6ccbfa62b04f70418bb
Surya K <surya.k@cloudsek.com>
2023-05-26 00:02:54 +0530

    Code for Scaling HTTP API with Docker and NGINX

---
+++ Scaling HTTP API Docker NGINX/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func rootPath(w http.ResponseWriter, r *http.Request){
+	fmt.Println("Request is in the Server")
+	io.WriteString(w, "You're here\n")
+}
+
+func handleRequests() {
+    http.HandleFunc("/", rootPath)
+    err := http.ListenAndServe("simpleapi:11011", nil)
+	if err !=nil{
+		fmt.Print(err)
+	}
+
+}
+
+func main() {
+	fmt.Print("HERE")
+    handleRequests()
+}
